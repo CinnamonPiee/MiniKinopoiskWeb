a1840d3f2fa8b43a78a093ab54627bbe3636ada0
CinnamonPie <vonahkahs.s.ved@gmail.com>
2024-07-23 12:37:25 +0400

    create migrations

--- mini_kinopoisk/users/models.py
+++ mini_kinopoisk/users/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 
 class Users(models.Model):
-    
+    name = models.CharField(max_length=60, null=True)
+    email = models.CharField(max_length=60, null=False, unique=True)
+    phone_number = models.CharField(max_length=15, null=False, unique=True)
+    telegram_id = models.BigIntegerField(null=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
